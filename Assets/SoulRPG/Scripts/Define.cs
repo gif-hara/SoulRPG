9edf954882f6d027e1cb95de6490a63365a378c1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-20 22:44:37 +0900

    feat: Add Define.cs file with Direction enum for managing game directions

---
+++ Assets/SoulRPG/Scripts/Define.cs
@@ -0,0 +1,16 @@
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Define
+    {
+        public enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+        }
+    }
+}
