5fcbcfbc37650b4074280ddf78a6a710a10fcc8f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-20 18:41:57 +0900

    feat: Add GameView class for managing game UI elements

---
+++ Assets/SoulRPG/Scripts/View/GameView.cs
@@ -0,0 +1,37 @@
+using System.Threading;
+using HK;
+using R3;
+using SoulRPG.CharacterControllers;
+using TMPro;
+using UnityEngine;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class GameView
+    {
+        private readonly HKUIDocument documentPrefab;
+
+        private readonly Character character;
+
+        public GameView(HKUIDocument documentPrefab, Character character)
+        {
+            this.documentPrefab = documentPrefab;
+            this.character = character;
+        }
+
+        public void Open(CancellationToken scope)
+        {
+            var document = Object.Instantiate(documentPrefab);
+            var positionText = document.Q<TMP_Text>("Text.Position");
+            character.PositionAsObservable()
+                .Subscribe(x =>
+                {
+                    positionText.text = $"Position: {x}";
+                })
+                .RegisterTo(scope);
+        }
+    }
+}
