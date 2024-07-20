d97a70641d3bb0290102b109f96ae26446a89ac4
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-20 22:38:49 +0900

    feat: Add lazy loading for images in GameSceneController

--- Assets/SoulRPG/Scripts/SceneControllers/GameSceneController.cs
+++ Assets/SoulRPG/Scripts/SceneControllers/GameSceneController.cs
@@ -1,3 +1,5 @@
+using HK;
+using SoulRPG.CharacterControllers;
 using UnityEngine;
 
 namespace SoulRPG.SceneControllers
@@ -7,8 +9,19 @@ namespace SoulRPG.SceneControllers
     /// </summary>
     public sealed class GameSceneController : MonoBehaviour
     {
-        void Start()
+        [SerializeField]
+        private HKUIDocument gameDocumentPrefab;
+
+        async void Start()
         {
+            await BootSystem.IsReady;
+            var player = new Character();
+            var gameView = new GameView(gameDocumentPrefab, player);
+            var playerController = new PlayerController();
+            var inputActions = new InputActions();
+            playerController.Attach(inputActions, player, destroyCancellationToken);
+            gameView.Open(destroyCancellationToken);
+            inputActions.Enable();
         }
     }
 }
