d8e780d46fc56d59c4849b955ec63b28030cdb42
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-20 23:16:26 +0900

    feat: Update player movement to include shift key functionality

--- Assets/SoulRPG/Scripts/CharacterControllers/PlayerController.cs
+++ Assets/SoulRPG/Scripts/CharacterControllers/PlayerController.cs
@@ -18,8 +18,21 @@ namespace SoulRPG
                 {
                     var v = x.ReadValue<Vector2>();
                     var velocity = new Vector2Int((int)v.x, (int)v.y);
-                    velocity = player.Direction.TransformVelocityByDirection(velocity);
-                    player.Position += new Vector2Int(velocity.x, velocity.y);
+                    if (inputActions.InGame.Shift.IsPressed())
+                    {
+                        if (!velocity.CanConvertToDirection())
+                        {
+                            return;
+                        }
+                        var direction = velocity.ToDirection();
+                        player.Direction = player.Direction.Rotate(direction);
+                    }
+                    else
+                    {
+
+                        velocity = player.Direction.TransformVelocityByDirection(velocity);
+                        player.Position += new Vector2Int(velocity.x, velocity.y);
+                    }
                 })
                 .RegisterTo(scope);
         }
