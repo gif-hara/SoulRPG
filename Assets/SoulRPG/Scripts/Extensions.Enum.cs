95bd0c626b4ef2f2623a92b47c704d50aa84b37f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-20 22:53:01 +0900

    feat: Add Extensions.Enum.cs file with extension methods for managing game directions

---
+++ Assets/SoulRPG/Scripts/Extensions.Enum.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static Vector2Int ToVector2Int(this Define.Direction self)
+        {
+            return self switch
+            {
+                Define.Direction.Up => new Vector2Int(0, 1),
+                Define.Direction.Right => new Vector2Int(1, 0),
+                Define.Direction.Down => new Vector2Int(0, -1),
+                Define.Direction.Left => new Vector2Int(-1, 0),
+                _ => throw new System.ArgumentOutOfRangeException()
+            };
+        }
+
+        public static Define.Direction ToLeft(this Define.Direction self)
+        {
+            return self switch
+            {
+                Define.Direction.Up => Define.Direction.Left,
+                Define.Direction.Right => Define.Direction.Up,
+                Define.Direction.Down => Define.Direction.Right,
+                Define.Direction.Left => Define.Direction.Down,
+                _ => throw new System.ArgumentOutOfRangeException()
+            };
+        }
+
+        public static Define.Direction ToRight(this Define.Direction self)
+        {
+            return self switch
+            {
+                Define.Direction.Up => Define.Direction.Right,
+                Define.Direction.Right => Define.Direction.Down,
+                Define.Direction.Down => Define.Direction.Left,
+                Define.Direction.Left => Define.Direction.Up,
+                _ => throw new System.ArgumentOutOfRangeException()
+            };
+        }
+
+        public static Define.Direction ToOpposite(this Define.Direction self)
+        {
+            return self switch
+            {
+                Define.Direction.Up => Define.Direction.Down,
+                Define.Direction.Right => Define.Direction.Left,
+                Define.Direction.Down => Define.Direction.Up,
+                Define.Direction.Left => Define.Direction.Right,
+                _ => throw new System.ArgumentOutOfRangeException()
+            };
+        }
+    }
+}
