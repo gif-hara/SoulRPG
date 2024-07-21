6ffb381e7971128d65eaaa72f2a06bbd57e1b4a2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-21 10:42:55 +0900

    Update MasterData asset to use SpreadSheetDungeonCellData for dungeon walls

--- Assets/SoulRPG/Scripts/MasterData.cs
+++ Assets/SoulRPG/Scripts/MasterData.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using HK;
 using UnityEngine;
@@ -12,8 +14,8 @@ namespace SoulRPG
     public sealed class MasterData : ScriptableObject
     {
         [SerializeField]
-        private SpreadSheetDungeonCellData.DictionaryList borders;
-        public SpreadSheetDungeonCellData.DictionaryList Borders => borders;
+        private DungeonWall.DictionaryList dungeonWalls;
+        public DungeonWall.DictionaryList DungeonWalls => dungeonWalls;
 
 #if UNITY_EDITOR
         [ContextMenu("Update")]
@@ -24,7 +26,35 @@ namespace SoulRPG
                 GoogleSpreadSheetDownloader.DownloadAsync("Dungeon.Test")
             );
 
-            borders.Set(JsonHelper.FromJson<SpreadSheetDungeonCellData>(database[0]));
+            var cellData = new SpreadSheetDungeonCellData.DictionaryList();
+            cellData.Set(JsonHelper.FromJson<SpreadSheetDungeonCellData>(database[0]));
+            var dw = new List<DungeonWall>();
+            foreach (var i in cellData.List)
+            {
+                if (i.borders.top)
+                {
+                    dw.Add(new DungeonWall { position = new Vector2Int(i.x, i.y) });
+                    dw.Add(new DungeonWall { position = new Vector2Int(i.x + 1, i.y) });
+                }
+                if (i.borders.bottom)
+                {
+                    dw.Add(new DungeonWall { position = new Vector2Int(i.x, i.y + 1) });
+                    dw.Add(new DungeonWall { position = new Vector2Int(i.x + 1, i.y + 1) });
+                }
+                if (i.borders.left)
+                {
+                    dw.Add(new DungeonWall { position = new Vector2Int(i.x, i.y) });
+                    dw.Add(new DungeonWall { position = new Vector2Int(i.x, i.y + 1) });
+                }
+                if (i.borders.right)
+                {
+                    dw.Add(new DungeonWall { position = new Vector2Int(i.x + 1, i.y) });
+                    dw.Add(new DungeonWall { position = new Vector2Int(i.x + 1, i.y + 1) });
+                }
+            }
+            dw = dw.Distinct().ToList();
+            dungeonWalls.Set(dw);
+
             UnityEditor.EditorUtility.SetDirty(this);
             UnityEditor.AssetDatabase.SaveAssets();
             Debug.Log("End MasterData Update");
@@ -57,6 +87,28 @@ namespace SoulRPG
             public bool left;
 
             public bool right;
+        }
+
+        [Serializable]
+        public class DungeonWall : IEquatable<DungeonWall>
+        {
+            public Vector2Int position;
+
+            public bool Equals(DungeonWall other)
+            {
+                return position == other.position;
+            }
+
+            public override int GetHashCode()
+            {
+                return position.GetHashCode();
+            }
+
+            [Serializable]
+            public sealed class DictionaryList : DictionaryList<Vector2Int, DungeonWall>
+            {
+                public DictionaryList() : base(x => x.position) { }
+            }
         }
     }
 }
