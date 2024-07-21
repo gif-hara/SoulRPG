42d84b04b859838bd2a72dfd007935b7aeb7c26a
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-21 19:16:30 +0900

    refactor: Add IsExistWall extension method to MasterData
    The commit adds the IsExistWall extension method to the MasterData class in the Extensions.MasterData.cs file. This method checks if a wall exists at a given position and direction in the dungeon. This change improves the functionality of the game by allowing for more accurate collision detection with walls.

---
+++ Assets/SoulRPG/Scripts/Extensions.MasterData.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static bool IsExistWall(this MasterData.Dungeon self, Vector2Int position, Define.Direction direction)
+        {
+            switch (direction)
+            {
+                case Define.Direction.Up:
+                    return self.wall.ContainsKey(position)
+                        && self.wall.ContainsKey(position + Vector2Int.right);
+                case Define.Direction.Down:
+                    return self.wall.ContainsKey(position + Vector2Int.down)
+                        && self.wall.ContainsKey(position + Vector2Int.down + Vector2Int.right);
+                case Define.Direction.Left:
+                    return self.wall.ContainsKey(position)
+                        && self.wall.ContainsKey(position + Vector2Int.down);
+                case Define.Direction.Right:
+                    return self.wall.ContainsKey(position + Vector2Int.right)
+                        && self.wall.ContainsKey(position + Vector2Int.right + Vector2Int.down);
+                default:
+                    Debug.LogError($"Invalid direction: {direction}");
+                    return false;
+            }
+        }
+    }
+}
