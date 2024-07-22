238277b5b0e078473496120a4ff12d428c2011e2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-23 01:49:43 +0900

    feat: Add Equipment class for managing character equipment

--- Assets/SoulRPG/Scripts/CharacterControllers/Equipment.cs
+++ Assets/SoulRPG/Scripts/CharacterControllers/Equipment.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using R3;
+
 namespace SoulRPG
 {
     /// <summary>
@@ -5,5 +8,52 @@ namespace SoulRPG
     /// </summary>
     public sealed class Equipment
     {
+        private readonly List<ReactiveProperty<int>> weaponIds = new();
+        public ReadOnlyReactiveProperty<int> WeaponIdAsObservable(int index) => weaponIds[index];
+
+        private readonly ReactiveProperty<int> headId = new();
+        public ReadOnlyReactiveProperty<int> HeadIdAsObservable() => headId;
+
+        private readonly ReactiveProperty<int> bodyId = new();
+        public ReadOnlyReactiveProperty<int> BodyIdAsObservable() => bodyId;
+
+        private readonly ReactiveProperty<int> armId = new();
+        public ReadOnlyReactiveProperty<int> ArmIdAsObservable() => armId;
+
+        private readonly ReactiveProperty<int> legId = new();
+        public ReadOnlyReactiveProperty<int> LegIdAsObservable() => legId;
+
+        private readonly List<ReactiveProperty<int>> accessoryIds = new();
+        public ReadOnlyReactiveProperty<int> AccessoryIdAsObservable(int index) => accessoryIds[index];
+
+        public void EquipWeapon(int index, int weaponId)
+        {
+            weaponIds[index].Value = weaponId;
+        }
+
+        public void EquipHead(int headId)
+        {
+            this.headId.Value = headId;
+        }
+
+        public void EquipBody(int bodyId)
+        {
+            this.bodyId.Value = bodyId;
+        }
+
+        public void EquipArm(int armId)
+        {
+            this.armId.Value = armId;
+        }
+
+        public void EquipLeg(int legId)
+        {
+            this.legId.Value = legId;
+        }
+
+        public void EquipAccessory(int index, int accessoryId)
+        {
+            accessoryIds[index].Value = accessoryId;
+        }
     }
 }
