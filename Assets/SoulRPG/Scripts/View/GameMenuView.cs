bb54b3bb19c017f84c9385db1ac0bd05b8b84cfb
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-23 23:44:19 +0900

    feat: Update game menu UI with new list elements and navigation
    This commit updates the `GameMenuView.cs` file to enhance the game menu UI. New list elements for "装備", "道具", "ステータス", and "システム" have been added, along with navigation functionality. The changes improve the functionality and visual appeal of the game menu.
    
    Co-authored-by: [Author Name] <[Author Email]>

--- Assets/SoulRPG/Scripts/View/GameMenuView.cs
+++ Assets/SoulRPG/Scripts/View/GameMenuView.cs
@@ -2,7 +2,10 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
 using R3;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace SoulRPG
 {
@@ -34,9 +37,19 @@ namespace SoulRPG
 
         private async UniTask StateRootMenuAsync(CancellationToken scope)
         {
-            var document = Object.Instantiate(documentBundlePrefab.Q<HKUIDocument>("UI.Game.Menu.Root"));
+            var document = Object.Instantiate(documentBundlePrefab.Q<HKUIDocument>("UI.Game.Menu.List"));
+            var leftAreaDocument = document.Q<HKUIDocument>("Area.Left");
+            var listParent = leftAreaDocument.Q<RectTransform>("Area.List");
+            var listElementPrefab = leftAreaDocument.Q<HKUIDocument>("ListElementPrefab");
             var inputController = TinyServiceLocator.Resolve<InputController>();
             var actions = inputController.InputActions.UI;
+
+            var firstElement = CreateListElement("装備");
+            CreateListElement("道具");
+            CreateListElement("ステータス");
+            CreateListElement("システム");
+            EventSystem.current.SetSelectedGameObject(firstElement.Q("Button"));
+
             actions.Cancel.OnPerformedAsObservable()
                 .Subscribe(_ =>
                 {
@@ -45,6 +58,19 @@ namespace SoulRPG
                 .RegisterTo(scope);
             await UniTask.WaitUntilCanceled(scope);
             Object.Destroy(document.gameObject);
+
+            HKUIDocument CreateListElement(string header)
+            {
+                var listElement = Object.Instantiate(listElementPrefab, listParent);
+                listElement.Q<TMP_Text>("Header").text = header;
+                listElement.Q<Button>("Button").OnClickAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        Debug.Log(header);
+                    })
+                    .AddTo(listElement);
+                return listElement;
+            }
         }
 
         private UniTask StateCloseAsync(CancellationToken scope)
