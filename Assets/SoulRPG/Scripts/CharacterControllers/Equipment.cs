e024c65944498b6fbc934b870eb497531fdb0f67
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-25 01:07:37 +0900

    feat: Add methods for retrieving weapon and accessory IDs in Equipment class
    This commit adds new methods to the Equipment class in the Equipment.cs file. The methods include `GetWeaponId`, `GetWeaponIds`, `GetAccessoryId`, and `GetAccessoryIds`, which allow retrieving the IDs of weapons and accessories respectively. These methods enhance the functionality of the code by providing convenient ways to access the IDs of equipment items.
    
    Co-authored-by: [Author Name] <[Author Email]>

--- Assets/SoulRPG/Scripts/CharacterControllers/Equipment.cs
+++ Assets/SoulRPG/Scripts/CharacterControllers/Equipment.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using R3;
 
 namespace SoulRPG
@@ -10,21 +11,29 @@ namespace SoulRPG
     {
         private readonly List<ReactiveProperty<int>> weaponIds = new();
         public ReadOnlyReactiveProperty<int> WeaponIdAsObservable(int index) => weaponIds[index];
+        public int GetWeaponId(int index) => weaponIds[index].Value;
+        public IEnumerable<int> GetWeaponIds() => weaponIds.Select(x => x.Value);
 
         private readonly ReactiveProperty<int> headId = new();
         public ReadOnlyReactiveProperty<int> HeadIdAsObservable() => headId;
+        public int HeadId => headId.Value;
 
         private readonly ReactiveProperty<int> bodyId = new();
         public ReadOnlyReactiveProperty<int> BodyIdAsObservable() => bodyId;
+        public int BodyId => bodyId.Value;
 
         private readonly ReactiveProperty<int> armId = new();
         public ReadOnlyReactiveProperty<int> ArmIdAsObservable() => armId;
+        public int ArmId => armId.Value;
 
         private readonly ReactiveProperty<int> legId = new();
         public ReadOnlyReactiveProperty<int> LegIdAsObservable() => legId;
+        public int LegId => legId.Value;
 
         private readonly List<ReactiveProperty<int>> accessoryIds = new();
         public ReadOnlyReactiveProperty<int> AccessoryIdAsObservable(int index) => accessoryIds[index];
+        public int GetAccessoryId(int index) => accessoryIds[index].Value;
+        public IEnumerable<int> GetAccessoryIds() => accessoryIds.Select(x => x.Value);
 
         public Equipment()
         {
