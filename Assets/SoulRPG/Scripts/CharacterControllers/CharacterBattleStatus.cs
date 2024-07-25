33d8eb97531d4d4a187c2523ab75ca03aeb178df
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-25 16:49:14 +0900

    CharacterBattleStatus

---
+++ Assets/SoulRPG/Scripts/CharacterControllers/CharacterBattleStatus.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class CharacterBattleStatus
+    {
+        [SerializeField]
+        private int hitPoint;
+        public int HitPoint => hitPoint;
+
+        [SerializeField]
+        private int stamina;
+        public int Stamina => stamina;
+        
+        [SerializeField]
+        private int physicalAttack;
+        public int PhysicalAttack => physicalAttack;
+        
+        [SerializeField]
+        private int magicalAttack;
+        public int MagicalAttack => magicalAttack;
+        
+        [SerializeField]
+        private float slashCutRate;
+        public float SlashCutRate => slashCutRate;
+        
+        [SerializeField]
+        private float blowCutRate;
+        public float BlowCutRate => blowCutRate;
+        
+        [SerializeField]
+        private float thrustCutRate;
+        public float ThrustCutRate => thrustCutRate;
+        
+        [SerializeField]
+        private float magicCutRate;
+        public float MagicCutRate => magicCutRate;
+        
+        [SerializeField]
+        private float fireCutRate;
+        public float FireCutRate => fireCutRate;
+        
+        [SerializeField]
+        private float thunderCutRate;
+        public float ThunderCutRate => thunderCutRate;
+        
+        [SerializeField]
+        private int speed;
+        public int Speed => speed;
+    }
+}
