853f783a314508825e351cea5616838a147ce939
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-25 17:31:03 +0900

    CharacterGrowthParameter

---
+++ Assets/SoulRPG/Scripts/CharacterControllers/CharacterGrowthParameter.cs
@@ -0,0 +1,23 @@
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class CharacterGrowthParameter
+    {
+        private int vitality;
+        public int Vitality => vitality;
+
+        public int stamina;
+        public int Stamina => stamina;
+
+        private int physicalStrength;
+        public int PhysicalStrength => physicalStrength;
+        
+        private int magicalStrength;
+        public int MagicalStrength => magicalStrength;
+        
+        private int speed;
+        public int Speed => speed;
+    }
+}
