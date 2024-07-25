36c0ce7eea3df91f5aebad1a739a7f80207091b3
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-25 20:22:21 +0900

    feat: Add methods to modify character growth parameters

--- Assets/SoulRPG/Scripts/CharacterControllers/CharacterGrowthParameter.cs
+++ Assets/SoulRPG/Scripts/CharacterControllers/CharacterGrowthParameter.cs
@@ -13,11 +13,36 @@ namespace SoulRPG
 
         private int physicalStrength;
         public int PhysicalStrength => physicalStrength;
-        
+
         private int magicalStrength;
         public int MagicalStrength => magicalStrength;
-        
+
         private int speed;
         public int Speed => speed;
+
+        public void AddVitality(int value)
+        {
+            vitality += value;
+        }
+
+        public void AddStamina(int value)
+        {
+            stamina += value;
+        }
+
+        public void AddPhysicalStrength(int value)
+        {
+            physicalStrength += value;
+        }
+
+        public void AddMagicalStrength(int value)
+        {
+            magicalStrength += value;
+        }
+
+        public void AddSpeed(int value)
+        {
+            speed += value;
+        }
     }
 }
