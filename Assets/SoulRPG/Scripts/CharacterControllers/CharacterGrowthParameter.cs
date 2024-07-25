30e2fbcd2f3c98e0768dcf4d40bdb2e66bdb12ce
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-25 20:55:37 +0900

    feat: Add CharacterGrowthParameter class for character growth parameters

--- Assets/SoulRPG/Scripts/CharacterControllers/CharacterGrowthParameter.cs
+++ Assets/SoulRPG/Scripts/CharacterControllers/CharacterGrowthParameter.cs
@@ -1,22 +1,32 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
 namespace SoulRPG
 {
     /// <summary>
     /// 
     /// </summary>
+    [Serializable]
     public sealed class CharacterGrowthParameter
     {
+        [SerializeField]
         private int vitality;
         public int Vitality => vitality;
 
+        [SerializeField]
         public int stamina;
         public int Stamina => stamina;
 
+        [SerializeField]
         private int physicalStrength;
         public int PhysicalStrength => physicalStrength;
 
+        [SerializeField]
         private int magicalStrength;
         public int MagicalStrength => magicalStrength;
 
+        [SerializeField]
         private int speed;
         public int Speed => speed;
 
