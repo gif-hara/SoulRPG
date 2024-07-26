99ac9d33c84d4fc22523fdba27ec30dc8eac2a99
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-27 00:39:23 +0900

    feat: Add input type change in BattleSystem BeginAsync method

--- Assets/SoulRPG/Scripts/BattleSystems/BattleSystem.cs
+++ Assets/SoulRPG/Scripts/BattleSystems/BattleSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using HK;
 using UnityEngine;
 
 namespace SoulRPG.BattleSystems
@@ -17,7 +18,10 @@ namespace SoulRPG.BattleSystems
         public async UniTask<Define.BattleResult> BeginAsync(CancellationToken scope)
         {
             Debug.Log("BattleSystem Begin");
+            var inputController = TinyServiceLocator.Resolve<InputController>();
+            inputController.ChangeInputType(InputController.InputType.UI);
             await UniTask.Delay(TimeSpan.FromSeconds(1.0f), cancellationToken: scope);
+            inputController.ChangeInputType(InputController.InputType.InGame);
             Debug.Log("BattleSystem End");
             return Define.BattleResult.PlayerWin;
         }
