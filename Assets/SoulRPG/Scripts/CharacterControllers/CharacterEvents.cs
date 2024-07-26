ff70aac052469892eae4b5b5332e1be8ece114d7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-27 00:17:24 +0900

    refactor: Add CharacterEvents class for handling character events

---
+++ Assets/SoulRPG/Scripts/CharacterControllers/CharacterEvents.cs
@@ -0,0 +1,12 @@
+using R3;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class CharacterEvents
+    {
+        public readonly Subject<(int itemId, int count)> OnAcquiredItem = new();
+    }
+}
