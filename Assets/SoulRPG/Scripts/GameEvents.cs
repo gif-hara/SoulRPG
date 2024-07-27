0124071fc4c92347b3c8db9257370ffdbf983190
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-27 21:57:05 +0900

    feat: Add WaitForSubmitInputAsync method to GameEvents.cs

--- Assets/SoulRPG/Scripts/GameEvents.cs
+++ Assets/SoulRPG/Scripts/GameEvents.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using R3;
 
 namespace SoulRPG
@@ -10,5 +11,14 @@ namespace SoulRPG
         public readonly Subject<string> OnRequestShowMessage = new();
 
         public readonly Subject<Unit> OnSubmitInput = new();
+
+        public readonly Subject<bool> OnRequestSetActiveMessageArrow = new();
+
+        public async UniTask WaitForSubmitInputAsync()
+        {
+            OnRequestSetActiveMessageArrow.OnNext(true);
+            await OnSubmitInput.FirstAsync();
+            OnRequestSetActiveMessageArrow.OnNext(false);
+        }
     }
 }
