699c369b097f02fabd25917bb7a4c66a8cb67860
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-27 16:32:32 +0900

    feat: Implement battle logic in BattleSystem BeginAsync method

--- Assets/SoulRPG/Scripts/BattleSystems/BattleSystem.cs
+++ Assets/SoulRPG/Scripts/BattleSystems/BattleSystem.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 namespace SoulRPG.BattleSystems
 {
@@ -11,19 +12,30 @@ namespace SoulRPG.BattleSystems
     /// </summary>
     public sealed class BattleSystem
     {
-        public BattleSystem()
-        {
-        }
-
-        public async UniTask<Define.BattleResult> BeginAsync(CancellationToken scope)
+        public static async UniTask<Define.BattleResult> BeginAsync(
+            BattleCharacter player,
+            BattleCharacter enemy,
+            CancellationToken scope
+            )
         {
             Debug.Log("BattleSystem Begin");
             var inputController = TinyServiceLocator.Resolve<InputController>();
             inputController.ChangeInputType(InputController.InputType.UI);
-            await UniTask.Delay(TimeSpan.FromSeconds(1.0f), cancellationToken: scope);
+
+            while (!player.BattleStatus.IsDead && !enemy.BattleStatus.IsDead)
+            {
+                player.BattleStatus.TakeDamage(enemy.BattleStatus.PhysicalAttack.CurrentValue);
+                Debug.Log($"Player HP: {player.BattleStatus.HitPoint.CurrentValue}");
+                await UniTask.Delay(TimeSpan.FromSeconds(1.0f), cancellationToken: scope);
+                enemy.BattleStatus.TakeDamage(player.BattleStatus.PhysicalAttack.CurrentValue);
+                Debug.Log($"Enemy HP: {enemy.BattleStatus.HitPoint.CurrentValue}");
+                await UniTask.Delay(TimeSpan.FromSeconds(1.0f), cancellationToken: scope);
+            }
+
+            var result = player.BattleStatus.IsDead ? Define.BattleResult.PlayerLose : Define.BattleResult.PlayerWin;
             inputController.ChangeInputType(InputController.InputType.InGame);
             Debug.Log("BattleSystem End");
-            return Define.BattleResult.PlayerWin;
+            return result;
         }
     }
 }
