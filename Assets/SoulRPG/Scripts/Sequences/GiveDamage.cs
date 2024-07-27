8b40d624e594a4bed063e41fdbb813a4fbf504a1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-27 21:15:53 +0900

    feat: Add GiveDamage sequence to apply damage to target character

---
+++ Assets/SoulRPG/Scripts/Sequences/GiveDamage.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using R3;
+using SoulRPG.BattleSystems.DamageCalculators;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class GiveDamage : ISequence
+    {
+        [SerializeReference, SubclassSelector]
+        private IDamageCalculator damageCalculator;
+
+        [SerializeField]
+        private bool waitForInput = true;
+
+        public async UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var actor = container.Resolve<BattleCharacter>("Actor");
+            var target = container.Resolve<BattleCharacter>("Target");
+            var weapon = container.Resolve<MasterData.Weapon>();
+            var damage = damageCalculator.Calculate(actor, target, weapon);
+            target.BattleStatus.TakeDamage(damage);
+            var message = $"{target.BattleStatus.Name}に{damage}のダメージ！";
+            TinyServiceLocator.Resolve<GameEvents>().OnRequestShowMessage.OnNext(message);
+            if (waitForInput)
+            {
+                await TinyServiceLocator.Resolve<GameEvents>().OnSubmitInput.FirstAsync();
+            }
+        }
+    }
+}
