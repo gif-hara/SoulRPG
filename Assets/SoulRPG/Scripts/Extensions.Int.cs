63fa370d241af6ed5632f5c81c51c3e76e9c4edd
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-27 20:09:16 +0900

    feat: Add GetMasterDataSkill extension method to Int class

--- Assets/SoulRPG/Scripts/Extensions.Int.cs
+++ Assets/SoulRPG/Scripts/Extensions.Int.cs
@@ -21,22 +21,22 @@ namespace SoulRPG
         {
             return TinyServiceLocator.Resolve<MasterData>().Weapons.TryGetValue(self, out weapon);
         }
-        
+
         public static bool TryGetMasterDataArmorHead(this int self, out MasterData.Armor armorHead)
         {
             return TinyServiceLocator.Resolve<MasterData>().ArmorHeads.TryGetValue(self, out armorHead);
         }
-        
+
         public static bool TryGetMasterDataArmorBody(this int self, out MasterData.Armor armorBody)
         {
             return TinyServiceLocator.Resolve<MasterData>().ArmorBodies.TryGetValue(self, out armorBody);
         }
-        
+
         public static bool TryGetMasterDataArmorArms(this int self, out MasterData.Armor armorArms)
         {
             return TinyServiceLocator.Resolve<MasterData>().ArmorArms.TryGetValue(self, out armorArms);
         }
-        
+
         public static bool TryGetMasterDataArmorLegs(this int self, out MasterData.Armor armorLegs)
         {
             return TinyServiceLocator.Resolve<MasterData>().ArmorLegs.TryGetValue(self, out armorLegs);
@@ -46,30 +46,35 @@ namespace SoulRPG
         {
             return TinyServiceLocator.Resolve<MasterData>().Weapons.ContainsKey(self);
         }
-        
+
         public static bool ContainsMasterDataArmorHead(this int self)
         {
             return TinyServiceLocator.Resolve<MasterData>().ArmorHeads.ContainsKey(self);
         }
-        
+
         public static bool ContainsMasterDataArmorBody(this int self)
         {
             return TinyServiceLocator.Resolve<MasterData>().ArmorBodies.ContainsKey(self);
         }
-        
+
         public static bool ContainsMasterDataArmorArms(this int self)
         {
             return TinyServiceLocator.Resolve<MasterData>().ArmorArms.ContainsKey(self);
         }
-        
+
         public static bool ContainsMasterDataArmorLegs(this int self)
         {
             return TinyServiceLocator.Resolve<MasterData>().ArmorLegs.ContainsKey(self);
         }
-        
+
         public static bool ContainsMasterDataAccessory(this int self)
         {
             return TinyServiceLocator.Resolve<MasterData>().Accessories.ContainsKey(self);
+        }
+
+        public static MasterData.Skill GetMasterDataSkill(this int self)
+        {
+            return TinyServiceLocator.Resolve<MasterData>().Skills.Get(self);
         }
     }
 }
