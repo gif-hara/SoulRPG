faf2ae37856445ff42ffa1f13e024a576cf1b4f0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-27 20:18:29 +0900

    feat: Add OnRequestShowMessage sequence for displaying messages during gameplay

---
+++ Assets/SoulRPG/Scripts/Sequences/OnRequestShowMessage.cs
@@ -0,0 +1,26 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class OnRequestShowMessage : ISequence
+    {
+        [SerializeField]
+        private string format;
+
+        public async UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var message = format
+                .Replace("{Actor}", container.Resolve<BattleCharacter>("Actor").BattleStatus.Name)
+                .Replace("{Target}", container.Resolve<BattleCharacter>("Target").BattleStatus.Name);
+            TinyServiceLocator.Resolve<GameEvents>().OnRequestShowMessage.OnNext(message);
+            await UniTask.Delay(1000, cancellationToken: cancellationToken);
+        }
+    }
+}
