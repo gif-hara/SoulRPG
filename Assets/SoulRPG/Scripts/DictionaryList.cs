9089fb87b78f1013254d172bb8e83d4ae9ed123d
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-28 15:08:20 +0900

    feat: Refactor DictionaryList.cs to use DictionaryData class

--- Assets/SoulRPG/Scripts/DictionaryList.cs
+++ Assets/SoulRPG/Scripts/DictionaryList.cs
@@ -7,96 +7,138 @@ using UnityEngine.Assertions;
 namespace HK
 {
     [Serializable]
-    public abstract class DictionaryList<TKey, TValue>
+    public sealed class DictionaryData<TKey, TValue>
     {
-        [SerializeField]
-        private List<TValue> list;
-
         private Dictionary<TKey, TValue> dictionary;
 
-        protected Func<TValue, TKey> idSelector;
+        private readonly Func<TValue, TKey> idSelector;
 
-        public IReadOnlyList<TValue> List => list;
-
-        public DictionaryList(Func<TValue, TKey> idSelector)
+        public DictionaryData(Func<TValue, TKey> idSelector)
         {
             this.idSelector = idSelector;
+            dictionary = null;
         }
 
         public void Set(IEnumerable<TValue> list)
         {
-            this.list = list.ToList();
-            dictionary = null;
+            dictionary ??= new Dictionary<TKey, TValue>();
+            dictionary.Clear();
+            foreach (var item in list)
+            {
+                dictionary.Add(idSelector(item), item);
+            }
         }
 
         public void Add(TValue value)
         {
-            list.Add(value);
-            if (dictionary != null)
-            {
-                dictionary.Add(idSelector(value), value);
-            }
+            dictionary.Add(idSelector(value), value);
         }
 
         public void Remove(TKey key)
         {
-            var value = dictionary[key];
-            list.Remove(value);
-            if (dictionary != null)
-            {
-                dictionary.Remove(key);
-            }
+            dictionary.Remove(key);
         }
 
         public void Remove(TValue value)
         {
             var key = idSelector(value);
-            list.Remove(value);
-            if (dictionary != null)
-            {
-                dictionary.Remove(key);
-            }
+            dictionary.Remove(key);
         }
 
         public void Clear()
         {
-            list.Clear();
-            dictionary = null;
+            dictionary.Clear();
         }
 
         public TValue Get(TKey key)
         {
-            InitializeIfNull();
             Assert.IsTrue(dictionary.ContainsKey(key), $"key={key}");
             return dictionary[key];
         }
 
         public bool ContainsKey(TKey key)
         {
-            InitializeIfNull();
             return dictionary.ContainsKey(key);
         }
 
         public bool TryGetValue(TKey key, out TValue value)
         {
-            InitializeIfNull();
             return dictionary.TryGetValue(key, out value);
+        }
+
+        public bool IsNull()
+        {
+            return dictionary == null;
+        }
+    }
+
+    [Serializable]
+    public abstract class DictionaryList<TKey, TValue>
+    {
+        [SerializeField]
+        private List<TValue> list;
+        public IReadOnlyList<TValue> List => list;
+
+        private DictionaryData<TKey, TValue> dictionaryData;
+
+        public DictionaryList(Func<TValue, TKey> idSelector)
+        {
+            dictionaryData = new DictionaryData<TKey, TValue>(idSelector);
+        }
+
+        public void Set(IEnumerable<TValue> list)
+        {
+            this.list = list.ToList();
+            dictionaryData.Set(list);
+        }
+
+        public void Add(TValue value)
+        {
+            list.Add(value);
+            dictionaryData.Add(value);
+        }
+
+        public void Remove(TValue value)
+        {
+            list.Remove(value);
+            dictionaryData.Remove(value);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+            dictionaryData.Clear();
+        }
+
+        public TValue Get(TKey key)
+        {
+            InitializeIfNull();
+            return dictionaryData.Get(key);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            InitializeIfNull();
+            return dictionaryData.ContainsKey(key);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            InitializeIfNull();
+            return dictionaryData.TryGetValue(key, out value);
         }
 
         private void InitializeIfNull()
         {
             // UnityEditorの場合は毎回初期化する
 #if UNITY_EDITOR
-            dictionary = null;
-#endif
-            if (dictionary == null)
+            dictionaryData.Set(list);
+#else
+            if (dictionaryData.IsNull())
             {
-                dictionary = new Dictionary<TKey, TValue>();
-                foreach (var item in list)
-                {
-                    dictionary.Add(idSelector(item), item);
-                }
+                dictionaryData.Set(list);
             }
+#endif
         }
     }
 }
