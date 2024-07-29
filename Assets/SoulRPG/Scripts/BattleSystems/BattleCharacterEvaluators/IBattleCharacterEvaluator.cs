186f49cc209aa303bec7490b036e9d362fbe5847
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-29 22:37:53 +0900

    feat: Add BattleCharacterEvaluators namespace and IBattleCharacterEvaluator interface
    This commit adds the BattleCharacterEvaluators namespace and the IBattleCharacterEvaluator interface. The namespace and interface are necessary to implement battle character evaluators in the game. The IBattleCharacterEvaluator interface defines a method to evaluate a battle character, and it will be used to provide different evaluation strategies for battle characters.
    
    Co-authored-by: [Author Name]

---
+++ Assets/SoulRPG/Scripts/BattleSystems/BattleCharacterEvaluators/IBattleCharacterEvaluator.cs
@@ -0,0 +1,10 @@
+namespace SoulRPG.BattleSystems.BattleCharacterEvaluators
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IBattleCharacterEvaluator
+    {
+        bool Evaluate(BattleCharacter battleCharacter);
+    }
+}
