99526dc23d527ff9d545a14c05aa6640f7b596d0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-30 15:37:55 +0900

    Ailment.101005

---
+++ Assets/SoulRPG/Scripts/BattleSystems/DamageCalculators/Constant.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+namespace SoulRPG.BattleSystems.DamageCalculators
+{
+    /// <summary>
+    /// 固定ダメージ計算
+    /// </summary>
+    [Serializable]
+    public sealed class Constant : IDamageCalculator
+    {
+        [SerializeField]
+        private int damage;
+        
+        public int Calculate(BattleCharacter attacker, BattleCharacter defender, MasterData.Weapon weapon)
+        {
+            return damage;
+        }
+    }
+}
