472f6ed62a4798153e118b93c372b6ff11bc7942
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-04 13:36:02 +0900

    feat: Add DialogView class for displaying dialog messages with options

---
+++ Assets/SoulRPG/Scripts/View/DialogView.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using R3;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DialogView
+    {
+        public static async UniTask<int> ConfirmAsync
+        (
+            HKUIDocument documentPrefab,
+            string message,
+            IEnumerable<string> options,
+            int defaultIndex,
+            CancellationToken scope
+        )
+        {
+            var source = new UniTaskCompletionSource<int>();
+            var document = Object.Instantiate(documentPrefab);
+            scope = CancellationTokenSource.CreateLinkedTokenSource(document.destroyCancellationToken, scope).Token;
+            document.Q<TMP_Text>("Message").text = message;
+            var elementParent = document.Q<Transform>("ElementParent");
+            var elementPrefab = document.Q<HKUIDocument>("ElementPrefab");
+            foreach (var e in options.Select((x, i) => (x, i)))
+            {
+                var element = Object.Instantiate(elementPrefab, elementParent);
+                element.Q<TMP_Text>("Header").text = e.x;
+                element.Q<Button>("Button").OnClickAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        source.TrySetResult(e.i);
+                    })
+                    .RegisterTo(scope);
+                if (defaultIndex == e.i)
+                {
+                    element.Q<Button>("Button").Select();
+                }
+            }
+            var result = await source.Task;
+            Object.Destroy(document.gameObject);
+            return result;
+        }
+    }
+}
