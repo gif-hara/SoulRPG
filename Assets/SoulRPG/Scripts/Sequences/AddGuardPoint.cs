18bfafee23754fb3aca6a94ab93f34a5b626eb6e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-07 17:19:51 +0900

    AddGuardPoint

---
+++ Assets/SoulRPG/Scripts/Sequences/AddGuardPoint.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class AddGuardPoint : ISequence
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeField]
+        private float rate;
+
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var key = targetType == Define.TargetType.Self ? "Actor" : "Target";
+            var actor = container.Resolve<BattleCharacter>(key);
+            var max = actor.BattleStatus.HitPointMax;
+            actor.BattleStatus.AddGuardPoint(Mathf.FloorToInt(max * rate));
+            return UniTask.CompletedTask;
+        }
+    }
+}
