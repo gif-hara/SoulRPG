e1b3043c0177f7d43a82c2bef9bfbabd0c0efc50
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-08 18:25:25 +0900

    RemoveAilment

---
+++ Assets/SoulRPG/Scripts/Sequences/RemoveAilment.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using SoulRPG.BattleSystems.BattleCharacterEvaluators;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class RemoveAilment : ISequence
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeField]
+        private int masterDataAilmentId;
+
+        [SerializeReference, SubclassSelector]
+        private IBattleCharacterEvaluator battleCharacterEvaluator;
+
+        public async UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            container.TryResolve<BattleCharacter>("Actor", out var actor);
+            container.TryResolve<BattleCharacter>("Target", out var target);
+            if ((actor != null && actor.BattleStatus.IsDead) || (target != null && target.BattleStatus.IsDead))
+            {
+                return;
+            }
+            if (battleCharacterEvaluator == null || battleCharacterEvaluator != null && battleCharacterEvaluator.Evaluate(actor, target))
+            {
+                var t = targetType == Define.TargetType.Self ? actor : target;
+                await t.AilmentController.RemoveAsync(masterDataAilmentId);
+            }
+        }
+    }
+}
