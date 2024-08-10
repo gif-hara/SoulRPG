676af31a49d03fa702fc026f581c429cfc9ed90d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-11 01:48:15 +0900

    feat: Add AudioManager script for handling game audio

---
+++ Assets/SoulRPG/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using HK;
+using UnityEngine;
+
+namespace MahCard
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class AudioManager : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource bgmSource;
+
+        [SerializeField]
+        private AudioSource sfxSource;
+
+        public static void PlayBGM(AudioClip clip)
+        {
+            var instance = TinyServiceLocator.Resolve<AudioManager>();
+            instance.bgmSource.clip = clip;
+            instance.bgmSource.Play();
+        }
+
+        public static void PlaySFX(AudioClip clip)
+        {
+            var instance = TinyServiceLocator.Resolve<AudioManager>();
+            instance.sfxSource.PlayOneShot(clip);
+        }
+
+        public static void StopBGM()
+        {
+            var instance = TinyServiceLocator.Resolve<AudioManager>();
+            instance.bgmSource.Stop();
+        }
+    }
+}
