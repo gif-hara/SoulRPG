f4fed46f150c9e7fe885b3931a01bda6e3a133df
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-10 23:46:58 +0900

    feat: Add AilmentBehaviourCount evaluator for evaluating ailment behaviour count

---
+++ Assets/SoulRPG/Scripts/BattleSystems/BattleCharacterEvaluators/AilmentBehaviourCount.cs
@@ -0,0 +1,46 @@
+using System;
+using SoulRPG.ContainerEvaluators;
+using UnityEngine;
+
+namespace SoulRPG.BattleSystems.BattleCharacterEvaluators
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class AilmentBehaviourCount : IBattleCharacterEvaluator
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeField]
+        private int masterDataAilmentId;
+
+        [SerializeField]
+        private int behaviourCount;
+
+        [SerializeField]
+        private IContainerEvaluator.CompareType compareType;
+
+        public bool Evaluate(BattleCharacter actor, BattleCharacter target)
+        {
+            var t = targetType == Define.TargetType.Self ? actor : target;
+            var ailmentElement = t.AilmentController.Find(masterDataAilmentId);
+            if (ailmentElement == null)
+            {
+                return false;
+            }
+
+            var count = ailmentElement.GetBehaviourCount();
+            return compareType switch
+            {
+                IContainerEvaluator.CompareType.Equals => count == behaviourCount,
+                IContainerEvaluator.CompareType.GreaterThan => count > behaviourCount,
+                IContainerEvaluator.CompareType.GreaterThanOrEquals => count >= behaviourCount,
+                IContainerEvaluator.CompareType.LessThan => count < behaviourCount,
+                IContainerEvaluator.CompareType.LessThanOrEquals => count <= behaviourCount,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+    }
+}
