f48b33180987133e5bba4008d72597f387de90c8
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-12 16:07:10 +0900

    feat: Update GameEnemyView to play OnTakeDamage animation
    This commit updates the GameEnemyView class in the GameEnemyView.cs file. It adds a subscription to the OnTakeDamage event of the enemy character, triggering the "Animation.OnTakeDamage" sequence when the enemy takes damage. This change ensures that the correct animation is played in response to the OnTakeDamage event.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

--- Assets/SoulRPG/Scripts/View/GameEnemyView.cs
+++ Assets/SoulRPG/Scripts/View/GameEnemyView.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
+using R3;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,10 +12,17 @@ namespace SoulRPG
     /// </summary>
     public sealed class GameEnemyView
     {
-        public static async UniTask OpenAsync(HKUIDocument documentPrefab, MasterData.Enemy masterDataEnemy, CancellationToken scope)
+        public static async UniTask OpenAsync(HKUIDocument documentPrefab, MasterData.Enemy masterDataEnemy, BattleCharacter enemy, CancellationToken scope)
         {
             var document = Object.Instantiate(documentPrefab);
+            var sequenceDocument = document.Q<HKUIDocument>("Sequences");
             document.Q<Image>("Image").sprite = masterDataEnemy.Thumbnail;
+            enemy.Events.OnTakeDamage
+                .Subscribe(_ =>
+                {
+                    sequenceDocument.Q<SequenceMonobehaviour>("Animation.OnTakeDamage").PlayAsync().Forget();
+                })
+                .RegisterTo(scope);
             await scope.WaitUntilCanceled();
             Object.Destroy(document.gameObject);
         }
