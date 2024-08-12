19fbcc14cd9e6fb1b2fcae45742091188f405799
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-12 12:27:51 +0900

    feat: Add HKUIDocument field to GameCameraController
    This commit adds a new HKUIDocument field to the GameCameraController class in the GameCameraController.cs file. The HKUIDocument is used to reference a UI document for playing animations asynchronously. This addition enhances the functionality of the GameCameraController by allowing it to play animations based on game events.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

--- Assets/SoulRPG/Scripts/GameCameraController.cs
+++ Assets/SoulRPG/Scripts/GameCameraController.cs
@@ -1,3 +1,6 @@
+using Cysharp.Threading.Tasks;
+using HK;
+using R3;
 using UnityEngine;
 
 namespace SoulRPG
@@ -7,5 +10,22 @@ namespace SoulRPG
     /// </summary>
     public sealed class GameCameraController : MonoBehaviour
     {
+        [SerializeField]
+        private HKUIDocument document;
+
+        void Start()
+        {
+            TinyServiceLocator.Resolve<GameEvents>().OnBeginBattle
+                .Subscribe(x =>
+                {
+                    x.Player.Events.OnTakeDamage
+                        .Subscribe(_ =>
+                        {
+                            document.Q<SequenceMonobehaviour>("Animation.TakeDamage").PlayAsync().Forget();
+                        })
+                        .RegisterTo(destroyCancellationToken);
+                })
+                .RegisterTo(destroyCancellationToken);
+        }
     }
 }
