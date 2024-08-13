6bf77d137d6bf12791ab98cb9e036a2e8bbbf231
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-13 23:12:55 +0900

    feat: Add Selector class for sequence evaluation
    This commit adds the Selector class to the Sequences namespace in the SoulRPG project. The Selector class implements the ISequence interface and provides a PlayAsync method for evaluating a list of elements and executing the corresponding sequences based on the evaluation result. This enhancement improves the sequence handling in the game.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

---
+++ Assets/SoulRPG/Scripts/Sequences/Selector.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class Selector : ISequence
+    {
+        [SerializeField]
+        private List<Element> elements;
+
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            foreach (var element in elements)
+            {
+                if (element.Evaluate.Evaluate(container))
+                {
+                    return new Sequencer(container, element.Sequences.Sequences).PlayAsync(cancellationToken);
+                }
+            }
+            Assert.IsTrue(false, "Not found any element");
+            return UniTask.CompletedTask;
+        }
+
+        [Serializable]
+        public sealed class Element
+        {
+            [SerializeReference, SubclassSelector]
+            private IEvaluate evaluate;
+            public IEvaluate Evaluate => evaluate;
+
+            [SerializeField]
+            private ScriptableSequences sequences;
+            public ScriptableSequences Sequences => sequences;
+        }
+    }
+}
