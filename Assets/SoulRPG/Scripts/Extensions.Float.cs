8c1e4e1db0c1f04238761e5243cc7b81b4803110
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-14 16:32:26 +0900

    feat: Add ToPercentage extension method to Float.cs

---
+++ Assets/SoulRPG/Scripts/Extensions.Float.cs
@@ -0,0 +1,13 @@
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static float ToPercentage(this float value)
+        {
+            return value * 100;
+        }
+    }
+}
