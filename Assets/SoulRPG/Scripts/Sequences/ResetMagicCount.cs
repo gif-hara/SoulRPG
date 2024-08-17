3fc0a4d7d41d5929ddf7a6908c1540d9a0351e50
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-18 04:17:37 +0900

    feat: Add ResetMagicCount sequence to reset magic count in SoulRPG
    This commit adds the ResetMagicCount sequence to the SoulRPG project. The sequence allows for resetting the magic count of a specific target, enhancing the gameplay experience.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

---
+++ Assets/SoulRPG/Scripts/Sequences/ResetMagicCount.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using SoulRPG.BattleSystems.BattleCharacterEvaluators;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class ResetMagicCount : ISequence
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeReference, SubclassSelector]
+        private IBattleCharacterEvaluator battleCharacterEvaluator;
+
+        public async UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var actor = container.Resolve<BattleCharacter>("Actor");
+            var target = container.Resolve<BattleCharacter>("Target");
+            if (actor.BattleStatus.IsDead || target.BattleStatus.IsDead)
+            {
+                return;
+            }
+            if (battleCharacterEvaluator == null || battleCharacterEvaluator != null && battleCharacterEvaluator.Evaluate(actor, target))
+            {
+                var t = targetType == Define.TargetType.Self ? actor : target;
+                t.BattleStatus.ResetMagicCount();
+                await TinyServiceLocator.Resolve<GameEvents>().ShowMessageAndWaitForSubmitInputAsync(new($"{t.BattleStatus.NameWithTag}の魔カウントがリセットされた", "Sfx.Message.0"));
+            }
+        }
+    }
+}
