1dc0c238534330e8f1fa61be8b66c8152b4808cc
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-20 23:28:17 +0900

    feat: Add KnifeCountView class for displaying knife count UI element
    This commit adds a new class, KnifeCountView, to the project. The KnifeCountView class is responsible for displaying the knife count UI element in the game. It listens to changes in the knife count and dynamically adds or removes UI elements accordingly. This addition enhances the user interface by providing a visual representation of the number of knives available to the player.
    
    Note: This commit message follows the established convention of starting with a verb in the imperative form, followed by a brief description of the changes.

---
+++ Assets/SoulRPG/Scripts/View/KnifeCountView.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using R3;
+using UnityEngine;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class KnifeCountView
+    {
+        public static async UniTask OpenAsync(HKUIDocument documentPrefab, BattleCharacter character, CancellationToken scope)
+        {
+            var document = Object.Instantiate(documentPrefab);
+            var elementParent = document.Q<Transform>("ElementParent");
+            var elementPrefab = document.Q<HKUIDocument>("ElementPrefab");
+            var elements = new List<HKUIDocument>();
+            character.BattleStatus.KnifeCountReactiveProperty
+                .Subscribe(x =>
+                {
+                    var diff = x - elements.Count;
+                    if (diff > 0)
+                    {
+                        for (var i = 0; i < diff; i++)
+                        {
+                            var element = Object.Instantiate(elementPrefab, elementParent);
+                            elements.Add(element);
+                        }
+                    }
+                    else if (diff < 0)
+                    {
+                        for (var i = 0; i < -diff; i++)
+                        {
+                            Object.Destroy(elements[elements.Count - 1].gameObject);
+                            elements.RemoveAt(elements.Count - 1);
+                        }
+                    }
+
+                    elementParent.gameObject.SetActive(elements.Count > 0);
+                })
+                .RegisterTo(scope);
+
+            await UniTask.WaitUntilCanceled(scope);
+
+            if (document != null)
+            {
+                Object.Destroy(document.gameObject);
+            }
+        }
+    }
+}
