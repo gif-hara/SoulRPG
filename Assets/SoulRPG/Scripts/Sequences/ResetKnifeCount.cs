b1380793b1d4b4e317bb2094bc2e46e7ee573820
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-20 23:54:00 +0900

    feat: Add ResetKnifeCount sequence for resetting knife count
    This commit adds a new sequence, ResetKnifeCount, to the project. The ResetKnifeCount sequence is responsible for resetting the knife count of a battle character. It checks if the actor and target are alive before resetting the knife count. This addition enhances the gameplay experience by providing a way to reset the number of knives available.
    
    Note: This commit message follows the established convention of starting with a verb in the imperative form, followed by a brief description of the changes.

---
+++ Assets/SoulRPG/Scripts/Sequences/ResetKnifeCount.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using SoulRPG.BattleSystems.BattleCharacterEvaluators;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class ResetKnifeCount : ISequence
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeReference, SubclassSelector]
+        private IBattleCharacterEvaluatorBoolean battleCharacterEvaluator;
+
+        public async UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var actor = container.Resolve<BattleCharacter>("Actor");
+            var target = container.Resolve<BattleCharacter>("Target");
+            if (actor.BattleStatus.IsDead || target.BattleStatus.IsDead)
+            {
+                return;
+            }
+            if (battleCharacterEvaluator == null || battleCharacterEvaluator != null && battleCharacterEvaluator.Evaluate(actor, target))
+            {
+                var t = targetType == Define.TargetType.Self ? actor : target;
+                t.BattleStatus.ResetKnifeCount();
+                await TinyServiceLocator.Resolve<GameEvents>().ShowMessageAndWaitForSubmitInputAsync(new($"{t.BattleStatus.NameWithTag}のナイフストックがリセットされた。", "Sfx.Message.0"));
+            }
+        }
+    }
+}
