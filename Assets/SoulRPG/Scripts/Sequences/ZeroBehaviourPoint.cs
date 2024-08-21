f3788cb73718e1acc39c6ce06fc3b83e939dfa61
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-21 17:44:44 +0900

    ZeroBehaviourPoint

---
+++ Assets/SoulRPG/Scripts/Sequences/ZeroBehaviourPoint.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using SoulRPG.BattleSystems.BattleCharacterEvaluators;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class ZeroBehaviourPoint : ISequence
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeReference, SubclassSelector]
+        private IBattleCharacterEvaluatorBoolean battleCharacterEvaluator;
+        
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var actor = container.Resolve<BattleCharacter>("Actor");
+            var target = container.Resolve<BattleCharacter>("Target");
+            if (actor.BattleStatus.IsDead || target.BattleStatus.IsDead)
+            {
+                return UniTask.CompletedTask;
+            }
+            if (battleCharacterEvaluator == null || battleCharacterEvaluator != null && battleCharacterEvaluator.Evaluate(actor, target))
+            {
+                var t = targetType == Define.TargetType.Self ? actor : target;
+                t.BattleStatus.AddBehaviourPoint(-t.BattleStatus.BehaviourPointReactiveProperty.CurrentValue);
+            }
+            
+            return UniTask.CompletedTask;
+        }
+    }
+}
