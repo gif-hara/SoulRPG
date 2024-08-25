6273b46eb260294a5f93a6fd8ea97473d4d8ffd7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-25 14:34:29 +0900

    feat: Update AudioManager to include audio mixer

--- Assets/SoulRPG/Scripts/AudioManager.cs
+++ Assets/SoulRPG/Scripts/AudioManager.cs
@@ -1,5 +1,6 @@
 using HK;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace SoulRPG
 {
@@ -13,6 +14,9 @@ namespace SoulRPG
 
         [SerializeField]
         private AudioSource sfxSource;
+
+        [SerializeField]
+        private AudioMixer audioMixer;
 
         public static void PlayBGM(AudioClip clip)
         {
@@ -31,6 +35,65 @@ namespace SoulRPG
         {
             var instance = TinyServiceLocator.Resolve<AudioManager>();
             instance.bgmSource.Stop();
+        }
+
+        public static void PlaySFX(string clipName)
+        {
+            var instance = TinyServiceLocator.Resolve<AudioManager>();
+            var clip = TinyServiceLocator.Resolve<GameRule>().AudioDatabase.Get(clipName).Clip;
+            instance.sfxSource.PlayOneShot(clip);
+        }
+
+        public static void SetVolumeMaster(float volume)
+        {
+            SetVolume("MasterVolume", volume);
+        }
+
+        public static void SetVolumeBGM(float volume)
+        {
+            SetVolume("BgmVolume", volume);
+        }
+
+        public static void SetVolumeSFX(float volume)
+        {
+            SetVolume("SfxVolume", volume);
+        }
+
+        public static float GetVolumeMaster()
+        {
+            return GetVolume("MasterVolume");
+        }
+
+        public static float GetVolumeBGM()
+        {
+            return GetVolume("BgmVolume");
+        }
+
+        public static float GetVolumeSFX()
+        {
+            return GetVolume("SfxVolume");
+        }
+
+        public static void SetVolume(string name, float volume)
+        {
+            var instance = TinyServiceLocator.Resolve<AudioManager>();
+            volume = Mathf.Clamp01(volume);
+            if (volume > 0)
+            {
+                volume = Mathf.Log10(volume) * 20;
+            }
+            else
+            {
+                volume = -80;
+            }
+            instance.audioMixer.SetFloat(name, volume);
+        }
+
+        public static float GetVolume(string name)
+        {
+            var instance = TinyServiceLocator.Resolve<AudioManager>();
+            instance.audioMixer.GetFloat(name, out var volume);
+            return Mathf.Pow(10, volume / 20);
         }
     }
 }
