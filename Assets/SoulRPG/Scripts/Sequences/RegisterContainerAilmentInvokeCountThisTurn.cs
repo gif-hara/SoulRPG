b542fac7e4557b57750b93fd46b9030fdc779bb6
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-29 23:06:25 +0900

    chore: Add RegisterContainerAilmentInvokeCountThisTurn sequence script

---
+++ Assets/SoulRPG/Scripts/Sequences/RegisterContainerAilmentInvokeCountThisTurn.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnitySequencerSystem;
+using UnitySequencerSystem.Resolvers;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class RegisterContainerAilmentInvokeCountThisTurn : ISequence
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeReference, SubclassSelector]
+        private IntResolver masterDataAilmentIdResolver;
+
+        [SerializeReference, SubclassSelector]
+        private StringResolver keyResolver;
+
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var key = targetType == Define.TargetType.Self ? "Actor" : "Target";
+            container.TryResolve<BattleCharacter>(key, out var actor);
+            var invokeTurnCountThisTurn = actor.AilmentController.GetInvokeCountThisTurn(masterDataAilmentIdResolver.Resolve(container));
+            Debug.Log($"RegisterContainerAilmentInvokeCountThisTurn: {key} {invokeTurnCountThisTurn}");
+            container.Register(keyResolver.Resolve(container), invokeTurnCountThisTurn);
+            return UniTask.CompletedTask;
+        }
+    }
+}
