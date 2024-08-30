ce44298585da791e447dcf3af90ce278b0c3b7ec
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-30 17:16:00 +0900

    RegisterContainerMagicCount

---
+++ Assets/SoulRPG/Scripts/Sequences/RegisterContainerMagicCount.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnitySequencerSystem;
+using UnitySequencerSystem.Resolvers;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class RegisterContainerMagicCount : ISequence
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeReference, SubclassSelector]
+        private StringResolver keyResolver;
+
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var key = targetType == Define.TargetType.Self ? "Actor" : "Target";
+            container.TryResolve<BattleCharacter>(key, out var actor);
+            container.Register(keyResolver.Resolve(container), actor.BattleStatus.MagicCount);
+            return UniTask.CompletedTask;
+        }
+    }
+}
