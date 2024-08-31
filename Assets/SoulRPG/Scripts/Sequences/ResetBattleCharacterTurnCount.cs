8051b3be5740592dabd6d30498f6ba500a55cbac
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-31 19:21:58 +0900

    feat: Add ResetBattleCharacterTurnCount sequence
    This commit adds a new sequence called `ResetBattleCharacterTurnCount` to the `Sequences` folder. The sequence is responsible for resetting the turn count of a battle character. It checks if the actor or target battle character is dead before resetting the turn count. This change ensures that the turn count is properly reset when needed.
    
    Co-authored-by: [Author Name]

---
+++ Assets/SoulRPG/Scripts/Sequences/ResetBattleCharacterTurnCount.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class ResetBattleCharacterTurnCount : ISequence
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        public async UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var actor = container.Resolve<BattleCharacter>("Actor");
+            var target = container.Resolve<BattleCharacter>("Target");
+            if (actor.BattleStatus.IsDead || target.BattleStatus.IsDead)
+            {
+                return;
+            }
+            var t = targetType == Define.TargetType.Self ? actor : target;
+            t.ResetTurnCount();
+        }
+    }
+}
