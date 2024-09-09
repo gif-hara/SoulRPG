b0231703ee00d6ed18e68d90a7a65e9cb03884b4
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-09-09 11:40:01 +0900

    GameNameInputFieldView

---
+++ Assets/SoulRPG/Scripts/View/GameNameInputFieldView.cs
@@ -0,0 +1,38 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using R3;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class GameNameInputFieldView
+    {
+        public static async UniTask<string> OpenAsync(HKUIDocument documentPrefab, CancellationToken scope)
+        {
+            var document = Object.Instantiate(documentPrefab);
+            var source = new UniTaskCompletionSource<string>();
+
+            document.Q<Button>("SubmitButton")
+                .OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    source.TrySetResult(document.Q<TMP_InputField>("InputField").text);
+                })
+                .RegisterTo(scope);
+            
+            var task = source.Task;
+            var winIndex = await UniTask.WhenAny(UniTask.WaitUntilCanceled(scope), task);
+            if (document != null)
+            {
+                Object.Destroy(document.gameObject);
+            }
+            return winIndex == 0 ? "" : await task;
+        }
+    }
+}
