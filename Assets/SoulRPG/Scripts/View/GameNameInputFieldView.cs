ccf5c0b1c1e025b082d7fe5b0fcde14382dd1dd3
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-09-10 02:15:41 +0900

    feat: Add input field functionality for game name in GameNameInputFieldView

--- Assets/SoulRPG/Scripts/View/GameNameInputFieldView.cs
+++ Assets/SoulRPG/Scripts/View/GameNameInputFieldView.cs
@@ -2,8 +2,10 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
 using R3;
+using R3.Triggers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace SoulRPG
@@ -17,15 +19,23 @@ namespace SoulRPG
         {
             var document = Object.Instantiate(documentPrefab);
             var source = new UniTaskCompletionSource<string>();
+            var inputField = document.Q<TMP_InputField>("InputField");
+            var submitButton = document.Q<Button>("Button.Submit");
 
-            document.Q<Button>("Button.Submit")
+            submitButton
                 .OnClickAsObservable()
                 .Subscribe(_ =>
                 {
-                    source.TrySetResult(document.Q<TMP_InputField>("InputField").text);
+                    source.TrySetResult(inputField.text);
                 })
                 .RegisterTo(document.destroyCancellationToken);
-
+            inputField.onEndEdit.AsObservable()
+                .Subscribe(_ =>
+                {
+                    EventSystem.current.SetSelectedGameObject(submitButton.gameObject);
+                })
+                .RegisterTo(document.destroyCancellationToken);
+            EventSystem.current.SetSelectedGameObject(inputField.gameObject);
             var task = source.Task;
             var winIndex = await UniTask.WhenAny(UniTask.WaitUntilCanceled(scope), task);
             if (document != null)
