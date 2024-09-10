86580f855110fc376ee9f6d29d41629fecac31c6
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-09-10 23:00:30 +0900

    feat: Add GameTitleScreenView to SoulRPG game menu

---
+++ Assets/SoulRPG/Scripts/View/GameTitleScreenView.cs
@@ -0,0 +1,34 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using R3;
+using UnityEngine;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class GameTitleScreenView
+    {
+        public static async UniTask OpenAsync(HKUIDocument documentPrefab, CancellationToken scope)
+        {
+            var anyClickScope = new CancellationTokenSource();
+            var document = Object.Instantiate(documentPrefab);
+            var sequences = document.Q<HKUIDocument>("Sequences");
+            var inputController = TinyServiceLocator.Resolve<InputController>();
+            document.Q<CanvasGroup>("Title").alpha = 0;
+            inputController.PushInputType(InputController.InputType.UI);
+            sequences.Q<SequenceMonobehaviour>("Animation.AnyClick.Loop").PlayAsync(anyClickScope.Token).Forget();
+            await inputController.InputActions.UI.Click.OnPerformedAsObservable().FirstAsync();
+            anyClickScope.Cancel();
+            anyClickScope.Dispose();
+            await sequences.Q<SequenceMonobehaviour>("Animation.In").PlayAsync(scope);
+            inputController.PopInputType();
+            if (document != null)
+            {
+                Object.Destroy(document.gameObject);
+            }
+        }
+    }
+}
