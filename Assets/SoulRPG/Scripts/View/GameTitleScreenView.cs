fc607b145002d35c36ec785b33cdc9c0e45b5b73
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-09-11 21:30:25 +0900

    feat: Update game title with player's sealed name

--- Assets/SoulRPG/Scripts/View/GameTitleScreenView.cs
+++ Assets/SoulRPG/Scripts/View/GameTitleScreenView.cs
@@ -2,6 +2,7 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
 using R3;
+using TMPro;
 using UnityEngine;
 
 namespace SoulRPG
@@ -17,7 +18,12 @@ namespace SoulRPG
             var document = Object.Instantiate(documentPrefab);
             var sequences = document.Q<HKUIDocument>("Sequences");
             var inputController = TinyServiceLocator.Resolve<InputController>();
+            var saveData = SaveData.LoadSafe();
+            var gameRule = TinyServiceLocator.Resolve<GameRule>();
             document.Q<CanvasGroup>("Area.Title").alpha = 0;
+            var title = gameRule.GameTitle;
+            title = title.Replace("{SealedName}", saveData.playerData.sealedName);
+            document.Q<TMP_Text>("Text.Title").text = title;
             inputController.PushInputType(InputController.InputType.UI);
             sequences.Q<SequenceMonobehaviour>("Animation.AnyClick.Loop").PlayAsync(anyClickScope.Token).Forget();
             await inputController.InputActions.UI.Click.OnPerformedAsObservable().FirstAsync();
