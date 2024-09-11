df492f0337bc07f5d769541b2303aae3b1af78a9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-09-11 17:23:29 +0900

    Enemy.19909

---
+++ Assets/SoulRPG/Scripts/Sequences/BeginBattle.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using SoulRPG.CharacterControllers;
+using UnityEngine;
+using UnitySequencerSystem;
+using UnitySequencerSystem.Resolvers;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class BeginBattle : ISequence
+    {
+        [SerializeReference, SubclassSelector]
+        private IntResolver enemyIdResolver;
+        
+        public async UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var result = await TinyServiceLocator.Resolve<DungeonController>().BeginBattleAsync(
+                TinyServiceLocator.Resolve<Character>("Player"),
+                enemyIdResolver.Resolve(container)
+            );
+            container.Register("BattleResult", result == Define.BattleResult.PlayerWin ? 0 : 1);
+        }
+    }
+}
