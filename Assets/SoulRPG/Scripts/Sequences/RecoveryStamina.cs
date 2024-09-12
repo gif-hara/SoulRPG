5916ea5590675b4c9678ec0da0319d7922fbc52b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-09-12 22:55:50 +0900

    Add RecoveryStamina sequence for stamina recovery in battles

---
+++ Assets/SoulRPG/Scripts/Sequences/RecoveryStamina.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using SoulRPG.BattleSystems.RecoveryCalculators;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnitySequencerSystem;
+
+namespace SoulRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class RecoveryStamina : ISequence
+    {
+        [SerializeReference, SubclassSelector]
+        private IRecoveryCalculator recoveryCalculator;
+
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeField]
+        private string sfxName = "Sfx.Message.19";
+
+        public async UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            container.TryResolve<BattleCharacter>("Actor", out var actor);
+            container.TryResolve<BattleCharacter>("Target", out var target);
+            if ((actor != null && actor.BattleStatus.IsDead) || (target != null && target.BattleStatus.IsDead))
+            {
+                return;
+            }
+            var gameEvents = TinyServiceLocator.Resolve<GameEvents>();
+            var t = targetType == Define.TargetType.Self ? actor : target;
+            Assert.IsNotNull(t, $"target is null targetType:{targetType}");
+            var fixedSfxName = string.IsNullOrEmpty(sfxName) ? "Sfx.Message.19" : sfxName;
+            var recovery = recoveryCalculator.Calculate(container);
+            if (recovery <= 0)
+            {
+                return;
+            }
+            else
+            {
+                Assert.IsNotNull(t, $"target is null targetType:{targetType}");
+                await UniTask.WhenAll(
+                    t.RecoveryStaminaAsync(recovery),
+                    gameEvents.ShowMessageAndWaitForSubmitInputAsync(new($"{t.BattleStatus.NameWithTag}のスタミナが<color=#88FF88>{recovery}</color>回復した。", fixedSfxName))
+                );
+            }
+        }
+    }
+}
