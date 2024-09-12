aa5b2333f7c7c02c360a4bc0d4f79384fe3c699b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-09-13 02:07:45 +0900

    Update GameEndCreditsView.cs to add animation sequence playback functionality

--- Assets/SoulRPG/Scripts/View/GameEndCreditsView.cs
+++ Assets/SoulRPG/Scripts/View/GameEndCreditsView.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
+using R3;
 using UnityEngine;
 
 namespace SoulRPG
@@ -10,10 +11,38 @@ namespace SoulRPG
     /// </summary>
     public sealed class GameEndCreditsView
     {
-        public static UniTask OpenAsync(HKUIDocument documentPrefab, CancellationToken scope)
+        public static async UniTask OpenAsync(HKUIDocument documentPrefab, CancellationToken scope)
         {
             var document = Object.Instantiate(documentPrefab);
-            return UniTask.Never(scope);
+            var sequences = document.Q<HKUIDocument>("Sequences");
+            var index = 0;
+            var inputScope = new CancellationTokenSource();
+            var inputController = TinyServiceLocator.Resolve<InputController>();
+            scope = CancellationTokenSource.CreateLinkedTokenSource(scope, inputScope.Token).Token;
+            try
+            {
+                inputController.InputActions.UI.Submit.OnPerformedAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        inputScope.Cancel();
+                        inputScope.Dispose();
+                    })
+                    .RegisterTo(scope);
+                while (true)
+                {
+                    var sequence = sequences.TryQ<SequenceMonobehaviour>($"Animation.In.{index}");
+                    if (sequence == null)
+                    {
+                        break;
+                    }
+                    await sequence.PlayAsync(scope);
+                    index++;
+                }
+            }
+            finally
+            {
+                Object.Destroy(document.gameObject);
+            }
         }
     }
 }
