45a765c8f7ade500d44003b8b52447f7097e29e1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-09-14 19:16:37 +0900

    エンドクレジットにスキップ機能を追加し、入力処理を改善

--- Assets/SoulRPG/Scripts/View/GameEndCreditsView.cs
+++ Assets/SoulRPG/Scripts/View/GameEndCreditsView.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
 using R3;
+using TMPro;
 using UnityEngine;
 
 namespace SoulRPG
@@ -13,21 +15,24 @@ namespace SoulRPG
     {
         public static async UniTask OpenAsync(HKUIDocument documentPrefab, CancellationToken scope)
         {
-            var document = Object.Instantiate(documentPrefab);
+            var document = UnityEngine.Object.Instantiate(documentPrefab);
             var sequences = document.Q<HKUIDocument>("Sequences");
+            var skipText = document.Q<TMP_Text>("Text.Skip");
             var index = 0;
             var inputScope = new CancellationTokenSource();
             var inputController = TinyServiceLocator.Resolve<InputController>();
+            var skipStateMachine = new TinyStateMachine();
             scope = CancellationTokenSource.CreateLinkedTokenSource(scope, inputScope.Token).Token;
             try
             {
-                inputController.InputActions.UI.Submit.OnPerformedAsObservable()
+                skipText.gameObject.SetActive(false);
+                TinyServiceLocator.Resolve<InputScheme>().AnyChangedAsObservable()
                     .Subscribe(_ =>
                     {
-                        inputScope.Cancel();
-                        inputScope.Dispose();
+                        skipText.text = inputController.InputActions.UI.Submit.GetTag() + " スキップ";
                     })
                     .RegisterTo(scope);
+                skipStateMachine.Change(StateAcceptSkipAsync);
                 while (true)
                 {
                     var sequence = sequences.TryQ<SequenceMonobehaviour>($"Animation.In.{index}");
@@ -41,7 +46,42 @@ namespace SoulRPG
             }
             finally
             {
-                Object.Destroy(document.gameObject);
+                UnityEngine.Object.Destroy(document.gameObject);
+            }
+
+            UniTask StateAcceptSkipAsync(CancellationToken _scope)
+            {
+                inputController.InputActions.UI.Submit.OnPerformedAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        skipStateMachine.Change(StateConfirmSkipAsync);
+                    })
+                    .RegisterTo(_scope);
+                return UniTask.CompletedTask;
+            }
+
+            async UniTask StateConfirmSkipAsync(CancellationToken _scope)
+            {
+                skipText.gameObject.SetActive(true);
+                await UniTask.NextFrame(cancellationToken: _scope);
+                inputController.InputActions.UI.Submit.OnPerformedAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        inputScope.Cancel();
+                        inputScope.Dispose();
+                    })
+                    .RegisterTo(_scope);
+
+                await UniTask.WhenAny
+                (
+                    UniTask.Delay(TimeSpan.FromSeconds(5.0f), cancellationToken: _scope),
+                    UniTask.WaitUntilCanceled(_scope)
+                );
+                if (skipText != null)
+                {
+                    skipText.gameObject.SetActive(false);
+                }
+                skipStateMachine.Change(StateAcceptSkipAsync);
             }
         }
     }
