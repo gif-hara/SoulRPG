dd62a4080066f42e68d6234b08ad4ebc7cc1d81c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-10-21 22:26:03 +0900

    Refactor Unity WebGL localization initialization in BootSystem.cs and Extensions.String.cs

---
+++ Assets/SoulRPG/Scripts/Editor/BuildSwitcher.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace SoulRPG.Editor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class BuildSwitcher
+    {
+        [MenuItem("SoulRPG/Build/UnityRoom")]
+        private static void SwitchToUnityRoom()
+        {
+            string[] symbols = new[]
+            {
+                "UNITY_POST_PROCESSING_STACK_V2",
+            };
+            PlayerSettings.SetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.WebGL, symbols);
+            Debug.Log("Switched to UnityRoom");
+        }
+
+        [MenuItem("SoulRPG/Build/itch.io")]
+        private static void SwitchToItchIo()
+        {
+            string[] symbols = new[]
+            {
+                "UNITY_POST_PROCESSING_STACK_V2",
+                "USE_LOCALIZATION",
+            };
+            PlayerSettings.SetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.WebGL, symbols);
+            Debug.Log("Switched to itch.io");
+        }
+    }
+}
